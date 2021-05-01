1ef76c712fabff9159693001cd5b6e9472143f46
Nikita <n.klimkin99@yandex.ru>
2021-05-01 14:15:55 +0700

    Initial commit

---
+++ src/main/java/com/nikita/klimkin/testTaskJarSoft/TestTaskJarSoftApplication.java
@@ -0,0 +1,13 @@
+package com.nikita.klimkin.testTaskJarSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskJarSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskJarSoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nikita/klimkin/testTaskJarSoft/TestTaskJarSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikita.klimkin.testTaskJarSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskJarSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
